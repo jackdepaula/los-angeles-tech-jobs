9cb9a655c7ca5bdbdd8825f828a09fbe160bd1ca
jackdepaula <jackdepaula@gmail.com>
2018-11-18 11:26:23 -0800

    add occupation data

--- Correlation.R
+++ Correlation.R
@@ -2,8 +2,8 @@ library(readxl)
 library(tidycensus)
 library(tidyverse)
 
-
-chci <- data.frame(read_excel("lacounty_zip_chci.xlsx", sheet="2009"))
+# chci <- data.frame(read_excel("./data/lacounty_zip_chci.xlsx", sheet="2009"))
+chci <- data.frame(read_excel("lacounty_zip_chci.xlsx", sheet="2012"))
 
 # Getting data from the 2012-2016 5-year ACS
 income <- get_acs(geography = "zcta", 
@@ -15,7 +15,7 @@ cor <- merge(chci, income, by.x = "zip", by.y = "GEOID")
 # Create the scatter plot with regression line
 
 p7 <- ggplot(cor, aes(x=chci, y=estimate)) +
-  geom_point(colour = "blue", size=5) + 
+  geom_point(colour = "blue", size=4) + 
   labs(title = "Correlation Between the CHCI and the\n
        Median House Income of Los Angeles County Zip Codes",
        x = "City Human Capital Index", y = "Median Household Income") +
@@ -24,5 +24,5 @@ p7 <- ggplot(cor, aes(x=chci, y=estimate)) +
               se=FALSE)    # Don't add shaded confidence region
 p7
 
-
-
+# lm.cor <- lm(estimate ~ chci, data = cor)
+# summary(lm.cor)
