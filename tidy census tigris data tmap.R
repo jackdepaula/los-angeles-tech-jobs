55f6416d82c385d8279de070f6bcda12cac5a64f
jackdepaula <jackdepaula@gmail.com>
2018-11-26 07:43:47 -0800

    reviewed/adjustments tmaps

--- tidy census tigris data tmap.R
+++ tidy census tigris data tmap.R
@@ -55,10 +55,16 @@ lacounty_data  <- fread("./data/lacountytech.csv")
 masterzip <- data.frame(read_excel("./data/MasterZipCodes.xlsx"))
 names(masterzip) <- c("zipcode", "name")
 
+# The shapefile format is a common way to store data for geographic areas, namely polygons, lines,
+# and points. Each tract is represented as a polygon (i.e. some shape) 
+
 # Dowload shape files for county from LA Data Portal
 # https://data.lacounty.gov/Geospatial/ZIP-Codes/65v5-jw9f
+
 zips.shp <- readOGR(("./data/zip-codes/geo_export_1887acad-adf8-4181-8c7d-50a16a3435f2.shp"))
 head(zips.shp@data)
+# my_spatial_polys <- SpatialPolygons(dt, proj4string = CRS("+proj=longlat +datum=WGS84") )
+
 masterzip$zipcode <- as.factor(masterzip$zipcode)
 str(masterzip)
 head(masterzip)
@@ -66,6 +72,20 @@ zip.shp <- inner_join(zips.shp@data, masterzip, by = "zipcode")
 dim(zip.shp)
 class(zip.shp)
 head(zip.shp)
+
+# How to Make Dot Density Maps
+# dot density maps are sometimes better 
+# for showing distributions within regions
+
+# The steps:
+
+# Install R if you haven't already and load necessary packages.
+# As usual, load and prepare your data.
+# Calculate dot placement.
+# Map results.
+# Customize to your needs.
+
+
 
 cnty.zips <- as.data.frame(zips.shp$zipcode)
 names(cnty.zips) <- c("GEOID")
@@ -91,91 +111,26 @@ df.17 <- df.17[order(df.17$GEOID),]
 # cnty.zips.17 <- subset(cnty.zips.17, is.element(cnty.zips.17$GEOID, df.17$GEOID))
 is.element(cnty.zips.17$GEOID, df.17$GEOID)
 
-
-
-
-
-# lacounty_data  <- fread("lacountytech.csv")
-
-## geting the state boundry file from package tigris
-# If you have issues with API, download from Tiger's Website
-# download.file("https://www.census.gov/geo/maps-data/data/cbf/cbf_state.html", desfile = 'states.zip")
-# unzip("states.zip")
-# state <- read_shape("cb_2015_us_state_20m.shp", as.sf = TRUE, stringsAsFactors = FALSE)
-
-# tigris_use_cache = TRUE
-# state<-states(cb=TRUE,class="sf")
-# head(state)
-# summary(state)
-# class(state)
-# # 
-# # ## geting the County boundry file from package tigris
-# tigris_use_cache = TRUE
-# county<-counties(cb=TRUE,class="sf")
-# head(county)
-# summary(county)
-# class(county)
-# 
-# ## geting the zip boundry file from package tigris
-# zip<-zctas(cb=TRUE,class="sf")
-# 
-# ## geting the zip boundry file from package tigris Only from LA
-# tigris_use_cache = TRUE
-# zip<-zctas(cb=TRUE, starts_with = c("90", "91", "92", "93"),class="sf")
-# 
-# head(zip)
-# summary(zip)
-# class(zip)
-
-
 ## joining data with geometry data to map the data
 str(zips.shp@data)
+head(zips.shp@data)
 str(df.17)
-
 zips.shp$zipcode <-as.numeric(zips.shp$zipcode)
 names(zips.shp)
-# state$GEOID <-as.numeric(state$GEOID)
-# state.sf<-inner_join(state,state.chci,by="GEOID")
 df.17$GEOID<-as.numeric(df.17$GEOID)
-# county.sf<-inner_join(county,county.chci,by="GEOID")
 
-# str(county.chci)
-# str(zip)
-# str(lacounty_data)
-# dim(lacounty_data)
-# dim(zip)
-# 
-# head(lacounty_data)
+# y <- merge(df.17, zips.shp, by.x = "GEOID", by.y = "zipcode")
 
-
-## for crearing a dataset for mapping the state with CHCI data point, we join two data frame zip.chci & zip
-# zip$ZCTA5CE10<-as.numeric(zip$ZCTA5CE10)
-# zip.sf<-inner_join(lacounty_data, zip,by=c("GEOID"="ZCTA5CE10"))
-
-y <- merge(df.17, zips.shp, by.x = "GEOID", by.y = "zipcode")
-
-# df.17.shp <-left_join(df.17, zips.shp,by=c("GEOID"="zipcode"))
-
-# zip.sf<-left_join(lacounty_data, zip,by=c("GEOID"="ZCTA5CE10"))
-
-# dt <- zip.sf
-# 
-# my_spatial_polys <- SpatialPolygons(dt, proj4string = CRS("+proj=longlat +datum=WGS84") )
-
-# Merge sf object and data frame
-# st_geometry(zip.sf) <- zip.sf$geometry
-# 
-# # Subset zip.sf for 2017
-# zip.sf.17 <- zip.sf %>% filter(year == 2017)
-# 
-# # # Subset zip.sf for 2015
-# zip.sf.05 <- zip.sf %>% filter(year == 2005)
 
 # #####################################################################################################
 # #
 # # Dot Density Maps using maptools
 # #
 # #####################################################################################################
+
+# dt <- zip.sf
+# 
+# my_spatial_polys <- SpatialPolygons(dt, proj4string = CRS("+proj=longlat +datum=WGS84") )
 
 library(maptools)
 
@@ -205,50 +160,109 @@ plot(zips.shp, lwd=0.1)
 # Plot a specific region
 plot(zips.shp, lwd=0.1, xlim=c(-118.510000,-117.820000), ylim=c(33.710000,34.860000))
 # plot(cadots.rand, add=T, pch=19, cex=0.1, col="#008800")
-plot(cadots.rand, add=T, pch=19, cex=0.1, col="#09900d")
+# plot(cadots.rand, add=T, pch=19, cex=0.1, col="#09900d") # Green
+plot(cadots.rand, add=T, pch=19, cex=0.1, col="#0000ff") # Blue
 
 
+# Use a different color for info and prof
+# races <- c("prof", "info")
+# dotCols <- c("#da6678", "#647eee") # "#c29219", "#09900d")
+# plot(zip.shp, lwd=0.1, xlim=c(-118.510000,-117.820000), ylim=c(33.710000,34.860000))
+# for (i in 2:length(races)) {
+#   nevar <- df0merge[,races[i]] / 10
+#   nedots.race <- dotsInPolys(zip.shp, as.integer(nevar), f="random")
+#   plot(nedots.race, add=TRUE, pch=19, cex=0.01, col=dotCols[i])
+# }
 
-##########
+
+################# 
+names(zips.shp@data)
+
+zip.sf.new <-left_join(df.17, zips.shp, by=c("GEOID"))
+#################
+
+###################  Using leaflet
+
+# Color Percentage
+# 
+qpal <- colorQuantile("Blues", df.17$per  , n = 10)
+
+map_test <- leaflet(zips.shp) %>%
+  setView(lng = -118.2437, lat = 34.2522, zoom = 10) %>%
+  addPolygons(stroke = FALSE,
+              smoothFactor = 0.2,
+              fillOpacity = 0.35,
+              color = ~qpal(df.17$per)) %>%
+  addProviderTiles(providers$Stamen.TonerLabels) %>%
+  leaflet::addLegend(pal = qpal, values = ~df.17$per,
+                     title = "2017 Tech Jobs", position = 'bottomleft')
+map_test
+
+
+# Tech - 10 bins
+
+qpal <- colorQuantile("Blues", df.17$tech  , n = 10)
+map_tech17 <- leaflet(zips.shp) %>%
+  setView(lng = -118.2437, lat = 34.2522, zoom = 10) %>%
+  addPolygons(stroke = FALSE,
+              smoothFactor = 0.2,
+              fillOpacity = 1,
+              color = ~qpal(df.17$tech)) %>%
+  leaflet::addLegend(pal = qpal, values = ~df.17$tech,
+                     title = "2017 Tech Jobs", opacity = 0.7, position = 'bottomleft')
+
+
+map_tech17
+
+
+# border color
+
+
+# #####################################################################################################
+# #
+# # Using census shape files
+# #
+# #####################################################################################################
+
+
+# lacounty_data  <- fread("./data/lacountytech.csv")
+
+## geting the state boundry file from package tigris
+# If you have issues with API, download from Tiger's Website
+# download.file("https://www.census.gov/geo/maps-data/data/cbf/cbf_state.html", desfile = 'states.zip")
+
+# ## geting the zip boundry file from package tigris
+zip<-zctas(cb=TRUE,class="sf")
+# 
+# ## geting the zip boundry file from package tigris Only from LA
+tigris_use_cache = TRUE
+zip<-zctas(cb=TRUE, starts_with = c("90", "91", "92", "93"),class="sf")
+
+state$GEOID <-as.numeric(state$GEOID)
+
+## for crearing a dataset for mapping the state with data point, we join two data frames
+zip$ZCTA5CE10<-as.numeric(zip$ZCTA5CE10)
+lacounty_data$GEOID <-as.numeric(zip$ZCTA5CE10)
+zip.sf<-left_join(lacounty_data, zip,by=c("GEOID"="ZCTA5CE10"))
+
+str(lacounty_data)
+
+# Merge sf object and data frame
+st_geometry(zip.sf) <- zip.sf$geometry
+ 
+# Subset zip.sf for 2017
+zip.sf.17 <- zip.sf %>% filter(year == 2017)
+
+# # # Subset zip.sf for 2015
+# zip.sf.05 <- zip.sf %>% filter(year == 2005)
+
 names(zip.shp) <- c("objectid","shape_area","shape_len","GEOID","name")
 df <- inner_join(zips.shp, lacounty_data, by = "GEOID")
-class(df.17)
+class(zip.sf.17)
 
-names(lacounty_data)
+################# TMAPS #########################
 
-tm_shape(zip.sf.17) + tm_fill()
-
-tm_shape(zip.sf.17) + tm_borders()
-
-map_la = tm_shape(zip.sf.17) +
-  tm_polygons() +
-  tm_fill(col = zip.sf.17$tech)
-map_la
-##########
-
-
-# new
-my_int_17 <- tm_shape(y) +
-  tm_polygons("per", id = "GEOID", n = 15, palette = "BuGn")
-tmap_mode('view')
-my_int_17 <- tmap_leaflet(my_int_17)
-my_int_17 %>% 
-  setView(lng = -118.2437, lat = 34.0522, zoom = 9) %>% 
-  addProviderTiles("CartoDB.Positron")
-
-
-
-
-# map 2017
-my_int_17 <- tm_shape(zip.sf.17) +
-  tm_polygons("per", id = "GEOID", n = 15, palette = "BuGn")
-tmap_mode('view')
-my_int_17 <- tmap_leaflet(my_int_17)
-my_int_17 %>% 
-  setView(lng = -118.2437, lat = 34.0522, zoom = 9) %>% 
-  addProviderTiles("CartoDB.Positron")
-
-# map
+# Standard settings
 map <- tm_shape(zip.sf.17) +
   tm_polygons("per", id = "GEOID")
 tmap_mode('view')
@@ -257,8 +271,17 @@ map %>%
   setView(lng = -118.2437, lat = 34.0522, zoom = 9) %>% 
   addProviderTiles("CartoDB.Positron")
 
+# Green Map 15 color bins
+my_int_17 <- tm_shape(zip.sf.17) +
+  tm_polygons("per", id = "GEOID", n = 15, palette = "BuGn")
+tmap_mode('view')
+my_int_17 <- tmap_leaflet(my_int_17)
+my_int_17 %>% 
+  setView(lng = -118.2437, lat = 34.0522, zoom = 9) %>% 
+  addProviderTiles("CartoDB.Positron")
+
+# Using Breaks
 breaks = c(0, 25, 40, 60, 75, 100)
-# map
 map <- tm_shape(zip.sf.17) +
   tm_polygons("per", id = "GEOID", breaks = breaks)
 tmap_mode('view')
@@ -267,16 +290,10 @@ map %>%
   setView(lng = -118.2437, lat = 34.0522, zoom = 9) %>% 
   addProviderTiles("CartoDB.Positron")
 
-# map
-map <- tm_shape(lacounty) +
-  tm_polygons()
-tmap_mode('view')
-map <- tmap_leaflet(map)
-map %>% 
-  setView(lng = -118.2437, lat = 34.0522, zoom = 9) %>% 
-  addProviderTiles("CartoDB.Positron")
+# tmap allows users to specify algorithms to automatically create breaks with the style argument
 
-# map
+
+# Using zips shape file (LA County)
 map <- tm_shape(zips.shp) +
   tm_polygons()
 tmap_mode('view')
@@ -286,9 +303,3 @@ map %>%
   addProviderTiles("CartoDB.Positron")
 
 
-
-
-# # Map color scale
-# breaks <- c(0, 2, 4, 6, 8, 10, 12, 14, 100)
-# pal <- colorRampPalette(c("#b9d7ef", "#102d44"))
-# col <- pal(length(breaks)-1)
